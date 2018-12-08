ab73d7f0279602ad6a4959ae9d8e243d8cc8fe28
Jonah Alexander-Awad <34581478+JonahAwad94@users.noreply.github.com>
2018-12-07 22:29:27 -0500

    Moved documentation to "README.md"

--- Prime Number Calculator/Prime Number Calculator.cpp
+++ Prime Number Calculator/Prime Number Calculator.cpp
@@ -1,20 +1,4 @@
-//////////////////////////////////////////////////////////////////////////////////////////////
-// Quick Math Program 2 - Prime Number Calculator                                           //
-//                                                                                          //
-// Created - 11/11/18 by Jonah Alexander-Awad                                               //
-// Last Modified - 11/29/18 by Jonah Alexander-Awad                                         //
-//                                                                                          //
-// NOTES:                                                                                   //
-// Technique used is dividing by all odd or even numbers up to square root of given number  //
-//                                                                                          //
-// Required calculations to decide if number is prime is as follows:                        //
-// under 25 (5^2) - 1                                                                       //
-// under 49 (7^2) - 2                                                                       //
-// under 81 (9^2) - 3                                                                       //
-// under 121(11^2)- 4                                                                       //
-// ...                                                                                      //
-//////////////////////////////////////////////////////////////////////////////////////////////
-
+// See "README.md"
 
 #include "pch.h" //For visual studios
 #include <iostream>
